57d37a74dc8e14ecfe89b7c133397b64cd58ff92
Saw Myat Phone Thwe <aura@Saws-MacBook-Air-2.local>
2026-08-04 09:20:07 +0700

    aura

--- webappdev/vite.config.js
+++ webappdev/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/midterm-pos-app',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'midterm.html'),
+      },
+    },
+  },
 })
